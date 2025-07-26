bc3908d9a7a8f36dac24a70ce927f1ef70d1e27c
Yusup Budiman <yusupbudiman3@gmail.com>
2025-07-26 16:51:22 +0700

    styling page home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,29 @@
 import { Link } from "react-router-dom";
+import Logo from "../assets/images/logo-chill.png";
+import User from "../assets/images/avatar.png";
+import { MdKeyboardArrowDown } from "react-icons/md";
 
 const Header = () => {
   return (
-    <header>
-      <nav className="flex gap-4">
-        <Link to="/login" className="hover:underline">
-          Home
+    <header className="flex justify-between items-center p-4 bg-gray-800 text-[10px] text-white ">
+      <nav className="flex gap-3 items-center">
+        <Link to="/start" className="">
+          <img src={Logo} alt="" />
         </Link>
-        <Link to="/about" className="hover:underline">
-          About
+        <Link to="/series" className="hover:underline">
+          Series
         </Link>
-        <Link to="/contact" className="hover:underline">
-          Contact
+        <Link to="/film" className="hover:underline">
+          Film
+        </Link>
+        <Link to="/mylist" className="hover:underline">
+          Daftar Saya
         </Link>
       </nav>
+      <div className="profile flex items-center gap-1">
+        <img src={User} alt="user" className="w-5 h5 rounded-xl" />
+        <MdKeyboardArrowDown className="w-4 h-4 font-bold" />
+      </div>
     </header>
   );
 };
