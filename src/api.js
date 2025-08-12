631bcc70819c59109ed7ad3060c715122f105498
YusupBudiman <yusupbudiman3@gmail.com>
2025-08-12 13:04:45 +0700

    configure api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", // json-server port
+  baseURL: "https://chill-movie-api.vercel.app", //"http://localhost:5000"
 });
 
 export default api;
