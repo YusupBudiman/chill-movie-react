d6a08be319094a00ed73b9f7a1fb9595f20c1c5b
YusupBudiman <yusupbudiman3@gmail.com>
2025-08-12 17:16:20 +0700

    clear error deployment

--- src/api.js
+++ src/api.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://chill-movie-backend-production.up.railway.app",
-  //"http://localhost:3000", // json-server port
 });
 
 export default api;
