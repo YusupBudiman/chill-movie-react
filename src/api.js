12a31c591240d2dd099f3f7fdca95d2cc07e8eea
YusupBudiman <yusupbudiman3@gmail.com>
2025-08-12 16:54:17 +0700

    fix error

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://chill-movie-backend-production.up.railway.app/", //"http://localhost:3000", // json-server port
+  baseURL: "https://chill-movie-backend-production.up.railway.app",
+  //"http://localhost:3000", // json-server port
 });
 
 export default api;
