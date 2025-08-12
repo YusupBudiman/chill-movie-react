a139b9b0e9a4b3fd7c8c6bad754c89cf8d21b20b
YusupBudiman <yusupbudiman3@gmail.com>
2025-08-12 14:51:44 +0700

    fixed error

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", // json-server port
+  baseURL: "http://localhost:3000", // json-server port
 });
 
 export default api;
